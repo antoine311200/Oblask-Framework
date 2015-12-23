51555639af01d73698f54de082055694073641b8
Antoine <NattackDev@users.noreply.github.com>
2015-12-23 15:38:41 +0100

    Create index.js

---
+++ plugin/oblask/api/index.js
@@ -0,0 +1,9 @@
+var fs = require("fs");
+SupCore.system.api.registerPlugin("typescript", "Oblask", {
+    code: fs.readFileSync(__dirname + "/Oblask.ts.txt", { encoding: "utf8" }),
+    defs: fs.readFileSync(__dirname + "/Oblask.d.ts.txt", { encoding: "utf8" })
+});
+SupCore.system.api.registerPlugin("typescript", "Oblask.Audio", {
+    code: fs.readFileSync(__dirname + "/Oblask.Audio.ts.txt", { encoding: "utf8" }),
+    defs: fs.readFileSync(__dirname + "/Oblask.Audio.d.ts.txt", { encoding: "utf8" })
+});
