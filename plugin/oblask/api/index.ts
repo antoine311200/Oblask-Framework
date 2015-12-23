ebafda2d08a09b289f0ee19ba06f36232f6a75a2
Antoine <NattackDev@users.noreply.github.com>
2015-12-23 15:55:05 +0100

    Update index.ts

--- plugin/oblask/api/index.ts
+++ plugin/oblask/api/index.ts
@@ -9,3 +9,8 @@ SupCore.system.api.registerPlugin("typescript", "Oblask.Audio", {
   code: fs.readFileSync(`${__dirname}/Oblask.Audio.ts.txt`, { encoding: "utf8" }),
   defs: fs.readFileSync(`${__dirname}/Oblask.Audio.d.ts.txt`, { encoding: "utf8" })
 });
+
+SupCore.system.api.registerPlugin("typescript", "Oblask.Interface", {
+  code: fs.readFileSync(`${__dirname}/Oblask.UI.ts.txt`, { encoding: "utf8" }),
+  defs: fs.readFileSync(`${__dirname}/Oblask.UI.d.ts.txt`, { encoding: "utf8" })
+});
