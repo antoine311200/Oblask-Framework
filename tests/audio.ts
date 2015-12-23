6209d65d4f7132ed4b675275d0d44b534ce62da7
Antoine <NattackDev@users.noreply.github.com>
2015-12-23 14:50:50 +0100

    Update audio.ts

--- tests/audio.ts
+++ tests/audio.ts
@@ -2,17 +2,28 @@ var playlist: Oblask.Audio.Playlist;
 
 class SetupBehavior extends Sup.Behavior {
   
+  //Awake method call on load
   awake() {
+    
+    //Create a playlist with tree sounds
     playlist = new Oblask.Audio.Playlist([
-      new Sup.Audio.SoundPlayer("Audio/Theme 1"), 
-      new Sup.Audio.SoundPlayer("Audio/Theme 2"), 
-      new Sup.Audio.SoundPlayer("Audio/Theme 3")
+      "Audio/Theme 1", 
+      "Audio/Theme 2", 
+      "Audio/Theme 3"
     ]);
   }
   
+  //Update method call 60 times per second (default)
   update() {
     if(Sup.Input.wasKeyJustPressed("A")) {
+      //Play the first sound "Audio/Theme 1"
       playlist.play(0);
+    }
+    
+    if(Sup.Input.wasKeyJustPressed("Z")) {
+      //Stop the first sound and play "Audio/Theme 2" and "Audio/Theme 3"
+      playlist.stop(0);
+      playlist.play([1, 2], true));
     }
   }
   
